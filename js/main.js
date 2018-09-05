6a37a13cb6a7b80eeaf084389f26ea1f77491442
Saptarshi Bhattacharya <bhattach@tarannon06.iai.uni-bonn.de>
2018-09-05 18:16:59 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+(function(){
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        World  = Matter.World,
+        Bodies = Matter.Bodies;
+
+    var engine = Engine.create();
+
+    var render = Render.create({
+        element: document.body,
+        engine: engine
+    });
+
+    var boxA = Bodies.rectangle(400,200,80,80);
+    var boxB = Bodies.rectangle(450, 50, 80,80);
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    shapes_array = [boxA, boxB, ground];
+    current_box = boxA;
+
+    World.add(engine.world, shapes_array);
+
+    document.addEventListener('keydown', function(event) {
+        if(event.keyCode == 39) {
+            force = { x: 0.2, y: 0 };
+            Matter.Body.applyForce(current_box, current_box.position, force);
+        }
+        if(event.keyCode == 37) {
+            force = { x: -0.2, y: 0 };
+            Matter.Body.applyForce(current_box, current_box.position, force);
+        }
+        if(event.keyCode == 38) {
+            force = { x: 0, y: -0.2 };
+            Matter.Body.applyForce(current_box, current_box.position, force);
+        }
+        if(event.keyCode == 32) {
+            var newbox = Bodies.rectangle(450, 50, 80,80);
+            shapes_array.push(newbox)
+            World.add(engine.world, newbox);
+            current_box = newbox;
+        }
+    });
+
+    Engine.run(engine);
+    Render.run(render);
+})()
