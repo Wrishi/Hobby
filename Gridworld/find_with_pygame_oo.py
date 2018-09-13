ffd0c5d3f26eb3cc32ef3a8b42a1b4ca9fb3b40a
SaptarshiBhattacharya <s6sabhat@uni-bonn.de>
2018-09-13 17:21:52 +0200

    Code cleanup and corrections

--- Gridworld/find_with_pygame_oo.py
+++ Gridworld/find_with_pygame_oo.py
@@ -27,7 +27,7 @@ default = {
 
 pg.init()
 class Agent:
-    def __init__(self, gridworld, position, actions = default["actions"], learning_rate = default["learning_rate"], 
+    def __init__(self, gridworld, position, actions = default["actions"], learning_rate = default["learning_rate"],
                 discount_factor = default["discount_factor"], episodes = default["episodes"], colors = default["agent_colors"]):
         print("Initializing agent...")
         self.gridworld = gridworld
@@ -57,7 +57,7 @@ class Agent:
 
 
         # For each episode
-        for episode in range(10000):
+        for episode in range(self.episodes):
             i,j = self.position
 
             # For each episode
@@ -113,7 +113,7 @@ class Agent:
                 if event.type == pg.QUIT:
                     run = False
 
-            
+
             self.move()
 
             x,y = self.position
@@ -138,7 +138,10 @@ class Gridworld:
     def readmap(self, mapfilename):
         gridworld = None
         with open(mapfilename, 'r') as file:
-            gridworld = [[int(digit) for digit in line.split(",")] for line in file.read().split("\n")]
+            text = file.read()
+            if text[len(text)-1] == "\n":
+                text = text[:len(text)-1]
+            gridworld = [[int(digit) for digit in line.split(",")] for line in text.split("\n")]
         gridworld = np.array(gridworld)
         print("Map loaded...")
         return gridworld
@@ -166,135 +169,5 @@ gw = Gridworld("./basemap.txt")
 agent = Agent(gridworld = gw, position = (1,1))
 agent.play()
 
-# run = True
-# while run:
-#     pg.time.delay(100)
-#     pg.draw.rect(win, (10,10,100), ((x+1) * dim, (y+1) * dim, dim, dim))
-#     pg.display.update()
-
-#     for event in pg.event.get():
-#         if event.type == pg.QUIT:
-#             run = False
-
-#     action = policy[x,y]
-#     x += actions[action][0]
-#     y += actions[action][1]
-
-#     if gridworld[x,y] in [2,3]:
-#         print("Out of the world.")
-#         run = False
-
-#     pg.draw.rect(win, (10,10,200), ((x+1) * dim, (y+1) * dim, dim, dim))
-    
-input()
-pg.quit()
-
-# with open('./basemap.txt', 'r') as file:
-#     # print( file.read().split("\n")[0])
-#     gridworld = [[int(digit) for digit in line.split(",")] for line in file.read().split("\n")]
-
-# gridworld = np.array(gridworld)
-# gridworld_x = len(gridworld)
-# gridworld_y = len(gridworld[0])
-# # print(gridworld)
-
-# dim = 10
-# pg.init()
-# win = pg.display.set_mode(((gridworld_x + 1) * dim,(gridworld_y + 1) * dim))
-# pg.display.set_caption("Find path")
-
-# for i in range(gridworld_x):
-#     for j in range(gridworld_y):
-#         color = (0,0,0)
-#         if gridworld[i,j] == 0:
-#             color = (24,24,24)
-#         elif gridworld[i,j] == 1:
-#             color = (200,10,10)
-#         elif gridworld[i,j] == 2:
-#             color = (10,200,10)
-
-#         size = ((i+1) * dim, (j+1) * dim, dim, dim)
-#         pg.draw.rect(win, color, size)
-#         pg.display.update()
-
-# x, y = 1, 1
-# pg.draw.rect(win, (10,10,200), ((x+1) * dim, (y+1) * dim, dim, dim))
-# pg.display.update()
-
-# rewards = {
-#     0: -1,
-#     1: -10,
-#     2: 100,
-#     3: -50
-# }
-
-# # E, W, S, N, SE, SW, NW, NE
-# actions = [( 1,  0), (-1,  0), ( 0,  1), ( 0, -1),
-# 		   ( 1,  1), ( 1, -1), ( 1,  1), (-1,  1)]
-# # a = list(actions.keys())
-
-# lr = 0.2
-# df = 0.8
-
-# # random action values
-# # Initialize Q arbitrarily
-# Q = np.zeros((gridworld_x, gridworld_y , len(actions)))
-
-
-# # For each episode
-# for episode in range(10000):
-#     # Initialise s
-#     # i = random.randint(1,9)
-#     # j = random.randint(1,9)
-#     i,j = 1,1
-
-#     # For each episode
-#     while True:
-#         # choose a from s using policy derived from Q
-#         # e-greedy
-#         e = 0.1
-#         if np.random.uniform() > e:
-#             action = np.argmax(Q[i,j])
-#         else:
-#             action = np.random.randint(0,4)
-
-#         # Take action
-#         # Observe next state
-#         i_n, j_n = i+actions[action][0], j+actions[action][1]
-#         # Observe Reward
-#         reward = rewards[gridworld[i_n, j_n]]
-
-#         Q[i][j][action] = (1 - lr) * Q[i][j][action] + lr * (reward + df * np.max(Q[i_n, j_n]))
-
-#         i, j = i_n, j_n
-
-#         if gridworld[i,j] in [2,3]:
-#             break;
-
-
-
-# policy = np.argmax(Q, axis= 2)
-# # print(policy.T)
-
-# run = True
-# while run:
-#     pg.time.delay(100)
-#     pg.draw.rect(win, (10,10,100), ((x+1) * dim, (y+1) * dim, dim, dim))
-#     pg.display.update()
-
-#     for event in pg.event.get():
-#         if event.type == pg.QUIT:
-#             run = False
-
-#     action = policy[x,y]
-#     x += actions[action][0]
-#     y += actions[action][1]
-
-#     if gridworld[x,y] in [2,3]:
-#         print("Out of the world.")
-#         run = False
-
-#     pg.draw.rect(win, (10,10,200), ((x+1) * dim, (y+1) * dim, dim, dim))
-    
 input()
 pg.quit()
